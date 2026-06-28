859f8db258e70b2aafdcb8618bea770462543f8f
Drmacze DEV <drma801@gmail.com>
2026-06-28 22:36:19 +0700

    Enable phase 3.9.1 polished shell

--- app/src/main/java/com/drmacze/f16launcher/DevLauncherActivity.kt
+++ app/src/main/java/com/drmacze/f16launcher/DevLauncherActivity.kt
@@ -41,7 +41,7 @@ class DevLauncherActivity : ComponentActivity() {
                             .background(Brush.linearGradient(listOf(Carbon, Color(0xFF071B2C), Carbon)))
                             .systemBarsPadding()
                     ) {
-                        Phase39FinalShell(api)
+                        Phase391FinalShell(api)
                     }
                 }
             }
